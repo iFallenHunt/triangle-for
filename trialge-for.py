cf4b6122b6d04418807b1db780194909ab028e57
Lucas Santos <lukas_santos.silva@hotmail.com>
2022-06-01 13:31:26 -0300

    I put some conditions so that the error in the insertion of the values is not possible

---
+++ trialge-for.py
@@ -0,0 +1,11 @@
+area = int
+base = int(input('Enter the base of the trinagle: '))
+height = int(input('Enter the height of the triangle: '))
+x = int
+z = int
+
+if base >= 0:
+    if height >=0:
+        area = base * height / 2
+
+print('The area of the triangle is:', area)
